2bb61f0721319691d03b6a11ae54e47d531d24e0
rachel-leee <56009283+rachel-leee@users.noreply.github.com>
2019-11-01 15:21:15 +0800

    Add files via upload

--- generateWindow_6.py
+++ generateWindow_6.py
@@ -1,10 +1,12 @@
 from tkinter import *
+import tkinter as tk
 import datetime
 import pandas as pd
 
 def generateWindow_6():
-    window_6 = Toplevel()
+    window_6 = Tk()
     window_6.title('Waiting time')
+    window_6.geometry('350x150+30+30')
 
     askingNumPeopleLabel = Label(window_6, text='Please enter number of people in the queue here for estimated waiting time.')
     askingNumPeopleLabel.grid(row=1,column=0,padx=2,pady=2)
@@ -12,3 +14,21 @@ def generateWindow_6():
     numPeopleEntry = Entry(window_6)
     numPeopleEntry.grid(row=1,column=1)
 
+    def getwaitTime():
+        numPeopleEntryStr = numPeopleEntry.get()
+        print(numPeopleEntryStr)
+
+
+        try:
+            numPeopleEntryInt = int(numPeopleEntryStr)
+            waitTime = numPeopleEntryInt*2
+
+        except ValueError:
+            status['text'] = 'Please check your input. Integer values only!'
+        else:
+            messagebox.showinfo(title="WaitTime", message="Please wait for "+str(waitTime)+" minutes.")
+
+
+
+    okwaitTimeButton = Button(window_6, padx=10, pady=5, bg='purple', fg='white', text='Ok', command=getwaitTime)
+    okwaitTimeButton.grid(row=2, column=1)
