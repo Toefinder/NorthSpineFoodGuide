f9ab28eeddbe32fca536c6aa8071ead7dc6b88d1
Quang Anh <43576719+Toefinder@users.noreply.github.com>
2019-11-01 10:59:53 +0800

    Add files via upload

--- generateWindow_5.py
+++ generateWindow_5.py
@@ -2,6 +2,29 @@ from tkinter import *
 import datetime
 import pandas as pd
 
-def generateWindow_5():
+operatingHours = pd.read_csv('operatingHours.csv')
+operatingHours = operatingHours.set_index('Stall') # set the column 'Stall' as index column
+def generateWindow_5(stallName):
     window_5 = Toplevel()
-    window_5.title('Operating time')
+    window_5.title(stallName)
+
+    descriptionLabel = Label(window_5, text='Operating time:')
+    descriptionLabel.grid(row=0, column=0)
+    
+    listDay = ['Weekdays', 'Saturday', 'Sunday']
+    for i in range(len(listDay)):
+        dayOfWeek = listDay[i]
+        dayLabel = Label(window_5, text=dayOfWeek)
+        dayLabel.grid(row=i+1, column=0)
+
+        openingTime = operatingHours.loc[stallName][operatingHours.loc[stallName, 'Day'] == dayOfWeek]['Opening Time'][0]
+        print(openingTime)
+
+        openingTimeLabel = Label(window_5, text=openingTime)
+        openingTimeLabel.grid(row=i+1, column=1)
+
+        closingTime = operatingHours.loc[stallName][operatingHours.loc[stallName, 'Day'] == dayOfWeek]['Closing Time'][0]
+        print(closingTime)
+
+        closingTimeLabel = Label(window_5, text=closingTime)
+        closingTimeLabel.grid(row=i+1, column=2)
