71056e1da1aaa28e697b8e1fc590bcc3b9801f26
rachel-leee <56009283+rachel-leee@users.noreply.github.com>
2019-11-01 15:28:26 +0800

    Add files via upload

--- generateWindow_6.py
+++ generateWindow_6.py
@@ -6,9 +6,9 @@ import pandas as pd
 def generateWindow_6():
     window_6 = Tk()
     window_6.title('Waiting time')
-    window_6.geometry('350x150+30+30')
+    window_6.geometry('550x100')
 
-    askingNumPeopleLabel = Label(window_6, text='Please enter number of people in the queue here for estimated waiting time.')
+    askingNumPeopleLabel = Label(window_6, text='Please enter number of people in the queue here for estimated waiting time:')
     askingNumPeopleLabel.grid(row=1,column=0,padx=2,pady=2)
 
     numPeopleEntry = Entry(window_6)
@@ -26,7 +26,7 @@ def generateWindow_6():
         except ValueError:
             status['text'] = 'Please check your input. Integer values only!'
         else:
-            messagebox.showinfo(title="WaitTime", message="Please wait for "+str(waitTime)+" minutes.")
+            messagebox.showinfo(title="Estimated Waiting Time", message="Please wait for "+str(waitTime)+" minutes.")
 
 
 
