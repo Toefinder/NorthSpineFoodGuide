19dcfd995a5a3ba930d7e08b133fe45cf0da8b07
Quang Anh <43576719+Toefinder@users.noreply.github.com>
2019-11-01 21:44:20 +0800

    Add files via upload

--- generateWindow_6.py
+++ generateWindow_6.py
@@ -8,16 +8,22 @@ def generateWindow_6():
     window_6.title('Waiting time')
     window_6.geometry('550x100')
 
-    askingNumPeopleLabel = Label(window_6, text='Please enter number of people in the queue here for estimated waiting time:')
+    topFrame = Frame(window_6)
+    topFrame.pack()
+
+    askingNumPeopleLabel = Label(topFrame, text='Please enter number of people in the queue here for estimated waiting time:')
     askingNumPeopleLabel.grid(row=1,column=0,padx=2,pady=2)
 
-    numPeopleEntry = Entry(window_6)
+    numPeopleEntry = Entry(topFrame)
     numPeopleEntry.grid(row=1,column=1)
 
-    def getwaitTime():
+    # *****status bar*****
+    status = Label(window_6, text = 'Entering...', bd = 1, relief = SUNKEN, anchor= W)
+    status.pack(side = BOTTOM, fill = X)
+
+    def getWaitTime():
         numPeopleEntryStr = numPeopleEntry.get()
         print(numPeopleEntryStr)
-
 
         try:
             numPeopleEntryInt = int(numPeopleEntryStr)
@@ -26,9 +32,12 @@ def generateWindow_6():
         except ValueError:
             status['text'] = 'Please check your input. Integer values only!'
         else:
-            messagebox.showinfo(title="Estimated Waiting Time", message="Please wait for "+str(waitTime)+" minutes.")
+            if numPeopleEntryInt < 0:
+                status['text'] = 'Please check your input. Integer value should be positive!'
+            else:
+                messagebox.showinfo(title="Estimated Waiting Time", message="Please wait for "+str(waitTime)+" minutes.")
 
 
 
-    okwaitTimeButton = Button(window_6, padx=10, pady=5, bg='purple', fg='white', text='Ok', command=getwaitTime)
+    okwaitTimeButton = Button(topFrame, padx=10, pady=5, bg='purple', fg='white', text='Ok', command=getWaitTime)
     okwaitTimeButton.grid(row=2, column=1)
